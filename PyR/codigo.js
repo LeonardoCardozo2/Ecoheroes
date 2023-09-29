100ceb3acfc127dcd7c0918d92e360831fa81ca3
Leonardo Cardozo <leonardocardozo153@gmail.com>
2023-09-29 10:34:37 -0300

    actualización

---
+++ PyR/codigo.js
@@ -0,0 +1,17 @@
+function mostrarDiv(){
+  document.getElementById("mensaje").style.top = "0";
+  document.getElementById("mensaje").style.zIndex = "50";
+}
+
+window.onload = function() {
+  var elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { // Firefox
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // Internet Explorer
+    elem.msRequestFullscreen();
+  }
+};
--- codigo.js
+++ codigo.js
@@ -1,5 +1,18 @@
 window.addEventListener('load', () =>{
-    const contenedor_loader = document.querySelector('.contenedor_loader')
+    const contenedor_loader = document.querySelector('section div')
     contenedor_loader.style.opacity = 0
     contenedor_loader.style.visibility = 'hidden'
 });
+
+window.onload = function() {
+  var elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { // Firefox
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // Internet Explorer
+    elem.msRequestFullscreen();
+  }
+};
